ce83567e03ec39e96bd0ba3b3f4a2788cbb0ec55
isseiyasuda <iseyasuda0520@icloud.com>
2021-04-06 18:02:19 +0900

    ユーザー登録テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :nickname , presence: true
-         validates :last_name , presence: true
-         validates :first_name , presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
+         validates :last_name , presence: true, format: { with: /\A[一-龥ぁ-ん]/,
+          message: "全角（漢字・ひらがな・カタカナ）のみ使えます" }
+         validates :first_name , presence: true,format: { with: /\A[一-龥ぁ-ん]/,
+          message: "全角（漢字・ひらがな・カタカナ）のみ使えます" }
+         validates :last_name_kana, presence: true,format: { with: /\A[ァ-ヶー－]+\z/,
+          message: "全角（カタカナ）のみ使えます" }
+         validates :first_name_kana, presence: true,format: { with: /\A[ァ-ヶー－]+\z/,
+          message: "全角（カタカナ）のみ使えます" }
+          validates :birth_date , presence: true
 end
