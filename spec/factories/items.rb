622c74d469ab70cbf97afdf13570318311e40ac0
isseiyasuda <iseyasuda0520@icloud.com>
2021-04-10 18:51:53 +0900

    エラーメッセージ等

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     product_name      {'xxx'}
     description       {'xxxx'}
-    category_id       {'2'}
-    condition_id      {'2'}
-    charge_id         {'2'}
-    area_id           {'2'}
-    delivery_day_id   {'2'}
-    price             {'55555'}
+    category_id       {2}
+    condition_id      {2}
+    charge_id         {2}
+    area_id           {2}
+    delivery_day_id   {2}
+    price             {55555}
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test.image.png'), filename: 'test_image.png')
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -17,47 +17,47 @@ RSpec.describe Item, type: :model do
         it '商品画像が空では登録できない' do
          @item.image = nil
          @item.valid?
-         expect(@item.errors.full_messages).to include()
+         expect(@item.errors.full_messages).to include("Image can't be blank")
         end
         it '商品名が空では出品できない' do
           @item.product_name = ''
           @item.valid?
-          expect(@item.errors.full_messages).to include()
+          expect(@item.errors.full_messages).to include("Product name can't be blank")
         end
         it '商品の説明が空では出品できない' do
           @item.description = ''
           @item.valid?
-          expect(@item.errors.full_messages).to include()
+          expect(@item.errors.full_messages).to include("Description can't be blank")
         end
         it 'カテゴリーの情報が空では出品できない' do
-          @item.product_name = ''
+          @item.category_id = '1'
           @item.valid?
-          expect(@item.errors.full_messages).to include()
+          expect(@item.errors.full_messages).to include("Category must be other than 1")
         end
         it '商品の状態の情報が空では出品できない' do
           @item.condition_id = '1'
           @item.valid?
-          expect(@item.errors.full_messages).to include()
+          expect(@item.errors.full_messages).to include("Condition must be other than 1")
         end
         it '配送料の負担の情報が空では出品できない' do
           @item.charge_id = '1'
           @item.valid?
-          expect(@item.errors.full_messages).to include()
+          expect(@item.errors.full_messages).to include("Charge must be other than 1")
         end
         it '発送元の地域についての情報が空では出品できない' do
           @item.area_id = '1'
           @item.valid?
-          expect(@item.errors.full_messages).to include()
+          expect(@item.errors.full_messages).to include("Area must be other than 1")
         end
         it '発送までの日数についての情報が空では出品できない' do
           @item.delivery_day_id = '1'
           @item.valid?
-          expect(@item.errors.full_messages).to include()
+          expect(@item.errors.full_messages).to include("Delivery day must be other than 1")
         end
         it '価格についての情報が空では出品できない' do
           @item.price = ''
           @item.valid?
-          expect(@item.errors.full_messages).to include()
+          expect(@item.errors.full_messages).to include("Price can't be blank")
         end
         it '価格の範囲が、¥300~¥9,999,999の間でなければ出品できない' do
           @item.price = '100'
@@ -67,12 +67,12 @@ RSpec.describe Item, type: :model do
         it '販売価格が英数では出品できない' do
           @item.price = 'abc'
           @item.valid?
-          expect(@item.errors.full_messages).to include()
+          expect(@item.errors.full_messages).to include("Price is not a number")
         end
         it '販売価格が全角では出品できない' do
           @item.price = '１２３４５'
           @item.valid?
-          expect(@item.errors.full_messages).to include()
+          expect(@item.errors.full_messages).to include("Price is not a number")
         end
     end
   end
