6dbbba0f4f9d1218cb23cecf98ffe506d23e1489
isseiyasuda <iseyasuda0520@icloud.com>
2021-04-12 17:34:07 +0900

    destroyアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create,:edit,:update]
-  before_action :set_item, only: [:show,:edit,:update]
+  before_action :set_item, only: [:show,:edit,:update,:destroy]
   before_action :redirect_root_path, only: [:edit,:update]
   def index
     @items = Item.all.order("created_at DESC")
@@ -30,6 +30,12 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
       redirect_to item_path 
     else render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_root_path
     end
   end
   
