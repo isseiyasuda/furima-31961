510624d660b2f53abe51e7e9c1d450c66802db48
isseiyasuda <iseyasuda0520@icloud.com>
2021-04-12 15:18:35 +0900

    editアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show,:edit]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -20,6 +20,9 @@ class ItemsController < ApplicationController
 
   def show
 
+  end
+
+  def edit
   end
   
   private
