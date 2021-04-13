8004fdb60e7c5255d2086ba4ca2e891de77abd22
isseiyasuda <iseyasuda0520@icloud.com>
2021-04-13 13:28:44 +0900

    テストコード実装

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :order do
-    
+    postal_code    {'123-4567'}
+    area_id    {2}
+    municipalities   {'テスト'}
+    street_num {'テスト1-1'}
+    phone_num{'00000000'}
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @order = FactoryBot.build(:order)
+  end
+  describe '商品購入' do
+    context '商品購入できるとき' do
+      it '郵便番号・都道府県・市区町村・番地・電話番号が存在すれば購入できる' do
+        expect(@order).to be_valid
+      end
+    end
+      context '商品購入できないとき' do
+        it '郵便番号が空では購入できない' do
+          @order.postal_code = ''
+      @order.valid?
+      expect(@order.errors.full_messages).to include("")
+        end
+        it '郵便番号はハイフンがなければ購入できない' do
+          @order.postal_code = '1234567'
+      @order.valid?
+      expect(@order.errors.full_messages).to include("")
+        end
+        it '都道府県が空では購入できない' do
+          @order.area_id = 1
+      @order.valid?
+      expect(@order.errors.full_messages).to include("")
+        end
+        it '市区町村が空では購入できない' do
+          @order.municipalities = ""
+      @order.valid?
+      expect(@order.errors.full_messages).to include("")
+        end
+        it '番地が空では購入できない' do
+          @order.street_num = ""
+      @order.valid?
+      expect(@order.errors.full_messages).to include("")
+        end
+        it '電話番号が空では購入できない' do
+          @order.street_num = ""
+      @order.valid?
+      expect(@order.errors.full_messages).to include("")
+        end
+        it '電話番号は12桁以上では購入できない' do
+          @order.street_num = "123456789123"
+      @order.valid?
+      expect(@order.errors.full_messages).to include("")
+        end
+        it '電話番号はハイフンがあると購入できない' do
+          @order.street_num = "1234−567"
+      @order.valid?
+      expect(@order.errors.full_messages).to include("")
+        end
+      end
+    end
+  end
+
